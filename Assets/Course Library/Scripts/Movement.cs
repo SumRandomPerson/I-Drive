5863e8008ba04d0a0ed06660455b8f91fcd7a48e
SumRandomPerson <113185567+SumRandomPerson@users.noreply.github.com>
2023-09-20 12:33:25 -0400

    changes

--- Assets/Course Library/Scripts/Movement.cs
+++ Assets/Course Library/Scripts/Movement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput; 
+    private float fowardInput; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +16,12 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
